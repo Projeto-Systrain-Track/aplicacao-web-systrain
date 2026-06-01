853a8b5d81a94d063d8cb9f9aa101ed91e9177c0
LeticiaPinheirooo <leticia.pinheiro@sptech.school>
2026-06-01 10:36:47 -0300

    habilitando a função de limparSessao

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -1,4 +1,4 @@
-/*
+
 // sessão
 
 function limparSessao() {
@@ -6,57 +6,57 @@ function limparSessao() {
     window.location = "../index.html";
 }
 
-// sessao usuario
+// // sessao usuario
 
-function validarSessaoUsuario() {
-    var nomeUser = sessionStorage.NOME_USUARIO
-    var emailUser = sessionStorage.EMAIL_USUARIO
-    var idUser = sessionStorage.ID_USUARIO
+// function validarSessaoUsuario() {
+//     var nomeUser = sessionStorage.NOME_USUARIO
+//     var emailUser = sessionStorage.EMAIL_USUARIO
+//     var idUser = sessionStorage.ID_USUARIO
 
-    if (nomeUser != null && emailUser != null && idUser != null) {
-        var identificacao = document.getElementById("nome_user");
-        var identificacaoMobile = document.getElementById("nome_user_m");
+//     if (nomeUser != null && emailUser != null && idUser != null) {
+//         var identificacao = document.getElementById("nome_user");
+//         var identificacaoMobile = document.getElementById("nome_user_m");
 
-        if (identificacao != null) {
-            identificacao.innerHTML = nomeUser;
-        }
-        if (identificacaoMobile != null) {
-            identificacaoMobile.innerHTML =  nomeUser;
-        }
+//         if (identificacao != null) {
+//             identificacao.innerHTML = nomeUser;
+//         }
+//         if (identificacaoMobile != null) {
+//             identificacaoMobile.innerHTML =  nomeUser;
+//         }
 
-    }else {
-        window.location = "../loginUsuario.html";
-    }
+//     }else {
+//         window.location = "../loginUsuario.html";
+//     }
 
-}
+// }
 
-// sessão adm
-function iniciarSessaoAdm(nome, email, nivel) {
-    sessionStorage.NOME_ADM = nome
-    sessionStorage.EMAIL_ADM = email
-    sessionStorage.NIVEL_ADM = nivel
-}
+// // sessão adm
+// function iniciarSessaoAdm(nome, email, nivel) {
+//     sessionStorage.NOME_ADM = nome
+//     sessionStorage.EMAIL_ADM = email
+//     sessionStorage.NIVEL_ADM = nivel
+// }
 
-function validarSessaoAdm() {
-    var nomeAdm = sessionStorage.NOME_ADM;
-    var emailAdm = sessionStorage.EMAIL_ADM;
-    var nivelAdm = sessionStorage.NIVEL_ADM;
+// function validarSessaoAdm() {
+//     var nomeAdm = sessionStorage.NOME_ADM;
+//     var emailAdm = sessionStorage.EMAIL_ADM;
+//     var nivelAdm = sessionStorage.NIVEL_ADM;
 
-    if (nomeAdm != null && emailAdm != null && nivelAdm != null) {
-        var identificacao = document.getElementById("nome_adm");
-        var identificacaoMobile = document.getElementById("nome_adm_m");
-        if (identificacao != null) {
-            identificacao.innerHTML = nomeAdm + " | N" + nivelAdm;
-        }
-        if (identificacaoMobile != null) {
-            identificacaoMobile.innerHTML = nomeAdm + " | N" + nivelAdm;
-        }
-    } else {
-        window.location = "../loginUsuario.html";
-    }
-}
-function limparDuvida() {
-    input_mensagem.value = "";
-    input_emailContato.value = "";
-}
-    */
+//     if (nomeAdm != null && emailAdm != null && nivelAdm != null) {
+//         var identificacao = document.getElementById("nome_adm");
+//         var identificacaoMobile = document.getElementById("nome_adm_m");
+//         if (identificacao != null) {
+//             identificacao.innerHTML = nomeAdm + " | N" + nivelAdm;
+//         }
+//         if (identificacaoMobile != null) {
+//             identificacaoMobile.innerHTML = nomeAdm + " | N" + nivelAdm;
+//         }
+//     } else {
+//         window.location = "../loginUsuario.html";
+//     }
+// }
+// function limparDuvida() {
+//     input_mensagem.value = "";
+//     input_emailContato.value = "";
+// }
+    
